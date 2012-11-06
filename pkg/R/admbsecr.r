79787143c38c5e52c5a1aa0c603122fbb85be048
b-steve <b-steve@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-11-06 14:44:37 +0000

    Updating to version 0.1.1.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/admbsecr@8 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/admbsecr.r
+++ pkg/R/admbsecr.r
@@ -1,5 +1,5 @@
 ## Package imports for roxygenise to pass to NAMESPACE.
-#' @import CircStats R2admb secr
+#' @import R2admb secr
 NULL
 
 #' Fitting SECR models in ADMB
@@ -67,7 +67,20 @@ NULL
 #'
 #'          \item sigmass:  As above.
 #'    }
+#'    \item \code{"dist"}: SECR with estimated distances between animals
+#'      and traps at which detections occurred. Parameters to estimate are:
+#'    \itemize{
+#'          \item D:        As above.
+#'
+#'          \item g0:       As above.
+#'
+#'          \item sigma:    As above.
+#'
+#'          \item alpha:    Shape parameter associated with the gamma distribution
+#'                          used to model estimated distances.
+#'    }
 #' }
+#'
 #' @param capt an array of dimension \code{(n, S, K)}, where \code{n} is the number of
 #' detected animals, \code{S} is number of individual sampling sessions, and \code{K}
 #' is the number of deployed traps. The object returned by  \code{make.capthist()} is
@@ -80,12 +93,20 @@ NULL
 #' @param traps a matrix containing the coordinates of trap locations. The object
 #' returned by \code{read.traps()} is suitable.
 #' @param mask a mask object. The object returned by \code{make.mask()} is suitable.
-#' @param sv either "auto", or a named vector of starting values for each of the model's
-#' parameters. See 'Details' for list of parameters used by each method. If a named
-#' vector, any of the elements can be set to \code{"auto"}, which will result in a
-#' (hopefully) sensible starting value being automatically generated for the parameters
-#' concerned. If \code{sv} itself is \code{"auto"}, all starting values are automatically
-#' generated.
+#' @param sv either \code{"auto"}, or a named vector. If \code{auto}, starting values for
+#' all parameters are automatically generated. If a vector, the elements are the starting
+#' values and the names indicate which parameter these correspond to. Starting values for
+#' all parameters need not be provided; they are automatically generated for any parameters
+#' that do not have a starting value explicitly provided. See 'Details' for list of
+#' parameters used by each method.
+#' @param bounds a list with optional components corresponding to parameters which are to
+#' have their default bounds overridden. Each component should be a vector of length two
+#' specifying the bounds, and the name of the component should be the name of the
+#' parameter to which these bounds apply. To remove default bounds from a parameter the
+#' component should be \code{NULL} rather than a vector of length two. Bounds for all
+#' parameters need not be provided; if there is no component corresponding to a
+#' parameter it keeps its default bounds. See 'Details' for list of parameters used by
+#' each method.
 #' @param ssqtoa an optional matrix. If calculated before call to \code{admbsecr},
 #' providing this will prevent recalculation.
 #' @param cutoff The signal strength threshold of detection. Required if \code{method} is
@@ -98,7 +119,7 @@ NULL
 #' @param memory value of \code{arrmblsize} in ADMB. Increase this if ADMB reports a
 #' memory error.
 #' @param profpars character vector of names of parameters over which profile likelihood
-#' should occur (untested and probably very slow!).
+#' should occur.
 #' @param clean logical, if \code{TRUE} ADMB files are cleaned after fitting of the model.
 #' @param verbose logical, if \code{TRUE} ADMB details, along with error messages, are
 #' printed to the R session.
@@ -106,17 +127,29 @@ NULL
 #' algorithm are printed to the R session.
 #' @param autogen logical, if \code{TRUE}, the appropriate \code{.tpl} file is written
 #' to \code{admbwd} (or the current working directory if \code{admbwd} is \code{NULL}).
-#' If \code{FALSE}, the \code{.tpl} file already be located in \code{admbwd} (or the
-#' current working directory if \code{admb} is \code{NULL}). Usually only set to
+#' If \code{FALSE}, the \code{.tpl} file should already be located in \code{admbwd} (or
+#' the current working directory if \code{admb} is \code{NULL}). Usually only set to
 #' \code{FALSE} for development purposes.
 #' @return An object of class 'admb'.
+#' 
+#' The following functions can be used to extract model components: \code{summary()},
+#' \code{AIC()}, \code{logLik()}, \code{deviance()}, \code{vcov()}, \code{coef()},
+#' \code{stdEr()}, and \code{confint()}.
+#'
+#' The latter takes arguments \code{level} and \code{method}, which specify the confidence
+#' level and calculation method respectively. The default method gives quadratic (Wald)
+#' intervals based on approximate standard errors; \code{"profile"} gives profile
+#' likelihood intervals, and can be used if the \code{admbsecr()} parameter
+#' \code{profpars} is non-null and provides names of model parameters that are to be
+#' profiled.
 #' @author Ben Stevenson
 #' @seealso \code{\link[R2admb]{do_admb}}, \code{\link[secr]{secr.fit}},
 #' \code{\link[secr]{make.capthist}}, \code{\link[secr]{read.traps}}.
 #' @export
-admbsecr <- function(capt, traps = NULL, mask, sv = "auto", ssqtoa = NULL, cutoff = NULL,
-                     admbwd = NULL, method = "simple", memory = NULL, profpars = NULL,
-                     clean = TRUE, verbose = FALSE, trace = FALSE, autogen = TRUE){
+admbsecr <- function(capt, traps = NULL, mask, sv = "auto", bounds = NULL,
+                     ssqtoa = NULL, cutoff = NULL, admbwd = NULL, method = "simple",
+                     memory = NULL, profpars = NULL, clean = TRUE, verbose = FALSE,
+                     trace = FALSE, autogen = TRUE){
   ## Warnings for incorrect input.
   if (length(method) != 1){
     stop("method must be of length 1")
@@ -135,6 +168,9 @@ admbsecr <- function(capt, traps = NULL, mask, sv = "auto", ssqtoa = NULL, cutof
   ## If traps is NULL, see if it is provided as part of capt.
   if (is.null(traps)){
     traps <- traps(capt)
+  }
+  if (diff(range(traps[, 1])) == 0 & diff(range(traps[, 2])) == 0 & any(sv == "auto")){
+    stop("All traps are at the same location; please provide starting values.")
   }
   ## Moving to ADMB working directory.
   if (!is.null(admbwd)){
@@ -161,42 +197,78 @@ admbsecr <- function(capt, traps = NULL, mask, sv = "auto", ssqtoa = NULL, cutof
   } else if (length(dim(bincapt)) > 4){
     stop("capt array cannot have more than 4 dimensions.")
   }
+  ## Parameter names.
+  parnames <- c("D", "g0"[!(method == "ss" | method == "sstoa")],
+                "sigma"[!(method == "ss" | method == "sstoa")],
+                "sigmatoa"[method == "toa" | method == "sstoa"],
+                "kappa"[method == "ang"],
+                "ssb0"[method == "ss" | method == "sstoa"],
+                "ssb1"[method == "ss" | method == "sstoa"],
+                "sigmass"[method == "ss" | method == "sstoa"],
+                "alpha"[method == "dist"])
   ## Setting number of model parameters.
-  npars <- c(3[method == "simple"], 4[method %in% c("toa", "ang", "ss")], 5[method == "sstoa"])
-  ## Setting sensible start values if elements of sv are "auto".
+  npars <- length(parnames)
+  ## Setting up bounds.
+  default.bounds <- list(D = c(0, 1e8),
+                         g0 = c(0, 1),
+                         sigma = c(0, 1e5),
+                         sigmatoa = c(0, 1e5),
+                         kappa = c(0, 700),
+                         ssb0 = NULL,
+                         ssb1 = c(-10, 0),
+                         sigmass = c(0, 1e5),
+                         alpha = c(0, 150))[parnames]
+  if (!(is.list(bounds) | is.null(bounds))){
+    stop("bounds must either be NULL or a list.")
+  } else {
+    bound.changes <- bounds
+    bounds <- default.bounds
+    for (i in names(default.bounds)){
+      if (i %in% names(bound.changes)){
+        bounds[[i]] <- bound.changes[[i]]
+      } else {
+        ## Removing NULL elements from list.
+        bounds[[i]] <- bounds[[i]]
+      }
+    }
+  }
+  ## If sv is a list, turn it into a vector.
+  if (is.list(sv)){
+    sv <- c(sv, recursive = TRUE)
+  }
+  ## Setting sv to a vector full of "auto" if required.
   if (length(sv) == 1 & sv[1] == "auto"){
     sv <- rep("auto", npars)
-  }
-  if (any(sv == "auto")){
-    ## Give sv vector names if it doesn't have them.
-    if (is.null(names(sv))){
-      names(sv) <- c("D", "g0"[!(method == "ss" | method == "sstoa")],
-                     "sigma"[!(method == "ss" | method == "sstoa")],
-                     "sigmatoa"[method == "toa" | method == "sstoa"],
-                     "kappa"[method == "ang"],
-                     "ssb0"[method == "ss" | method == "sstoa"],
-                     "ssb1"[method == "ss" | method == "sstoa"],
-                     "sigmass"[method == "ss" | method == "sstoa"])
-    } else {
-      ## Reordering sv vector if names are provided.
-      sv <- sv[c("D", "g0"[!(method == "ss" | method == "sstoa")],
-                 "sigma"[!(method == "ss" | method == "sstoa")],
-                 "sigmatoa"[method == "toa" | method == "sstoa"],
-                 "kappa"[method == "ang"],
-                 "ssb0"[method == "ss" | method == "sstoa"],
-                 "ssb1"[method == "ss" | method == "sstoa"],
-                 "sigmass"[method = "ss" | method == "sstoa"])]
+    names(sv) <- parnames
+  } else if (is.null(names(sv))){
+    stop("sv is not a named vector.")
+  } else if (length(unique(sv)) != length(sv)){
+    stop("sv names are not all unique")
+  } else {
+    ## Warning if a listed parameter name is not used in this model.
+    if (!all(names(sv) %in% parnames)){
+      warning("One of the element names of sv is not a parameter used in this model.")
     }
-    autofuns <- list("D" = autoD, "g0" = autog0, "sigma" = autosigma,
-                     "sigmatoa" = autosigmatoa, "kappa" = autokappa,
-                     "ssb0" = autossb0, "ssb1" = autossb1,
-                     "sigmass" = autosigmass)
-    ## Replacing "auto" elements of sv vector.
-    for (i in rev(which(sv == "auto"))){
-      sv[i] <- autofuns[[names(sv)[i]]](capt, bincapt, traps, mask, sv, cutoff, method)
+    sv.old <- sv
+    sv <- rep("auto", npars)
+    names(sv) <- parnames
+    for (i in parnames){
+      if (any(names(sv.old) == i)){
+        sv[i] <- sv.old[i]
+      }
     }
-    sv <- as.numeric(sv)
+    ## Reordering sv vector.
+    sv <- sv[parnames]
   }
+  autofuns <- list("D" = autoD, "g0" = autog0, "sigma" = autosigma,
+                   "sigmatoa" = autosigmatoa, "kappa" = autokappa,
+                   "ssb0" = autossb0, "ssb1" = autossb1,
+                   "sigmass" = autosigmass, "alpha" = autoalpha)
+  ## Replacing "auto" elements of sv vector.
+  for (i in rev(which(sv == "auto"))){
+    sv[i] <- autofuns[[names(sv)[i]]](capt, bincapt, traps, mask, sv, cutoff, method)
+  }
+  sv <- as.numeric(sv)
   ## Removing attributes from capt and mask objects as do_admb cannot handle them.
   bincapt <- matrix(as.vector(bincapt), nrow = n, ncol = k)
   capt <- array(as.vector(capt), dim = c(n, k, dim(capt)[4][length(dim(capt)) == 4]))
@@ -211,7 +283,6 @@ admbsecr <- function(capt, traps = NULL, mask, sv = "auto", ssqtoa = NULL, cutof
     data <- list(n = n, ntraps = k, nmask = nm, A = A, capt = capt,
                  dist = dist, traps = traps, trace = trace)
     params <- list(D = sv[1], g0 = sv[2], sigma = sv[3])
-    bounds <- list(D = c(0, 10000000), g0 = c(0, 1), sigma = c(0, 100000))
   } else if (method == "toa"){
     if (is.null(ssqtoa)){
       ssqtoa <- apply(capt, 1, toa.ssq, dists = dist)
@@ -219,20 +290,15 @@ admbsecr <- function(capt, traps = NULL, mask, sv = "auto", ssqtoa = NULL, cutof
     data <- list(n = n, ntraps = k, nmask = nm, A = A, toacapt = capt,
                  toassq = t(ssqtoa), dist = dist, capt = bincapt, trace = trace)
     params <- list(D = sv[1], g0 = sv[2], sigma = sv[3], sigmatoa = sv[4])
-    bounds <- list(D = c(0, 10000000), g0 = c(0, 1), sigma = c(0, 100000),
-                   sigmatoa = c(0, 100000))
   } else if (method == "ang"){
     angs <- angles(traps, mask)
     data <- list(n = n, ntraps = k, nmask = nm, A = A, angcapt = capt,
                  ang = angs, dist = dist, capt = bincapt, trace = trace)
     params <- list(D = sv[1], g0 = sv[2], sigma = sv[3], kappa = sv[4])
-    bounds <- list(D = c(0, 10000000), g0 = c(0, 1), sigma = c(0, 100000),
-                   kappa = c(0, 700))
   } else if (method == "ss"){
     data <- list(n = n, ntraps = k, nmask = nm, A = A, c = cutoff, sscapt = capt,
                  dist = dist, capt = bincapt, trace = trace)
     params <- list(D = sv[1], ssb0 = sv[2], ssb1 = sv[3], sigmass = sv[4])
-    bounds <- list(D = c(0, 10000000), sigmass = c(0, 100000), ssb1 = c(-100000, 0))
   } else if (method == "sstoa"){
     if (is.null(ssqtoa)){
       ssqtoa <- apply(capt[, , 1], 1, toa.ssq, dists = dist)
@@ -241,15 +307,17 @@ admbsecr <- function(capt, traps = NULL, mask, sv = "auto", ssqtoa = NULL, cutof
                  toacapt = capt[, , 2], toassq = t(ssqtoa), dist = dist, capt = bincapt,
                  trace = trace)
     params <- list(D = sv[1], sigmatoa = sv[2], ssb0 = sv[3], ssb1 = sv[4], sigmass = sv[5])
-    bounds <- list(D = c(0, 10000000), sigmass = c(0, 100000), ssb1 = c(-100000, 0),
-                   sigmatoa = c(0, 100000))
+  } else if (method == "dist"){
+    data <- list(n = n, ntraps = k, nmask = nm, A = A, distcapt = capt, dist = dist,
+                 capt = bincapt, trace = trace)
+    params <- list(D = sv[1], g0 = sv[2], sigma = sv[3], alpha = sv[4])
   } else {
-    stop('method must be either "simple", "toa", "ang", "ss", or "sstoa"')
+    stop('method must be either "simple", "toa", "ang", "ss", "sstoa", or "dist"')
   }
   ## Fitting the model.
   if (!is.null(profpars)){
     fit <- do_admb(prefix, data = data, params = params, bounds = bounds, verbose = verbose,
-                   profile = FALSE, profpars = profpars, safe = TRUE,
+                   profile = TRUE, profpars = profpars, safe = FALSE,
                    run.opts = run.control(checkdata = "write", checkparam = "write",
                      clean_files = clean))
   } else {
--- pkg/R/autofuns.r
+++ pkg/R/autofuns.r
@@ -1,10 +1,13 @@
 ## Functions that automatically generate starting values for parameters.
+
+## Lifted from the secr package.
 autosigma <- function(capthist = NULL, bincapt, traps, mask, sv = NULL, cutoff = NULL,
                       method = NULL){
   obsRPSV <- RPSV.mod(bincapt, traps)
   secr:::naivesigma(obsRPSV, traps, mask, 0, 1)
 }
 
+## Lifted from the secr package.
 autoD <- function(capthist = NULL, bincapt, traps, mask, sv, cutoff = NULL, method = NULL){
   n <- dim(bincapt)[1]
   A <- attr(mask, "area")
@@ -19,21 +22,33 @@ autoD <- function(capthist = NULL, bincapt, traps, mask, sv, cutoff = NULL, meth
               list(g0 = g0, sigma = sigma), 1))*A)
 }
 
+## Don't think we can estimate this in advance for a single session.
 autog0 <- function(capthist = NULL, bincapt = NULL, traps = NULL, mask = NULL, sv = NULL,
                    cutoff = NULL, method = NULL){
   0.95
 }
 
+## Take average std dev across all individuals' TOAs. This will be an
+## overestimate as it assumes sound travels instantaneously. Does not
+## seem to work well with frog data.
 autosigmatoa <- function(capthist = NULL, bincapt = NULL, traps = NULL, mask = NULL, sv = NULL,
                          cutoff = NULL, method = NULL){
+  ## if (method == "sstoa"){
+  ##   capthist <- capthist[, , , 2, drop = FALSE]
+  ## }
+  ## mean(apply(capthist, 1, function(x) sd(x[x != 0])), na.rm = TRUE)
   0.0025
 }
 
+## Haven't come up with a good way for this yet.
 autokappa <- function(capthist = NULL, bincapt = NULL, traps = NULL, mask = NULL, sv = NULL,
                       cutoff = NULL, method = NULL){
   10
 }
 
+## Assumes signal strength received is equal to signal strength
+## produced. Mean of received signal strengths is then converted to a
+## mean for a truncated normal.
 autossb0 <- function(capthist, bincapt = NULL, traps = NULL, mask = NULL, sv = NULL,
                      cutoff, method){
   if (method == "sstoa"){
@@ -44,14 +59,20 @@ autossb0 <- function(capthist, bincapt = NULL, traps = NULL, mask = NULL, sv = N
   sigma <- sd(ss)
   alpha <- (cutoff - mu)/sigma
   lambda <- dnorm(alpha)/(1 - pnorm(alpha))
-  mu + sigma*lambda
+  log(mu + sigma*lambda)
 }
 
+## Assumes signal strength received is almost equal to signal strength
+## produced. Given a small negative value to get away from the
+## paramter bound.
 autossb1 <- function(capthist = NULL, bincapt = NULL, traps = NULL, mask = NULL, sv = NULL,
                      cutoff = NULL, method = NULL){
-  -0.1
+  -0.01
 }
 
+## Assumes signal strength received is equal to signal strength
+## produced. Std dev of received signal strengths is then converted to
+## a std dev for a truncated normal.
 autosigmass <- function(capthist, bincapt = NULL, traps = NULL, mask = NULL, sv = NULL,
                         cutoff, method = NULL){
   if (method == "sstoa"){
@@ -64,6 +85,11 @@ autosigmass <- function(capthist, bincapt = NULL, traps = NULL, mask = NULL, sv 
   lambda <- dnorm(alpha)/(1 - pnorm(alpha))
   delta <- lambda*(lambda - alpha)
   sqrt(sigma^2*(1 - delta))
+}
+
+autoalpha <- function(capthist, bincapt = NULL, traps = NULL, mask = NULL, sv = NULL,
+                       cutoff = NULL, method = NULL){
+  2
 }
 
 ## Following are helper functions for naive sigma estimation.
